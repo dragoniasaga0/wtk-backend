8916f0bb9dd955d461ec86b97173b720d89220d5
dragoniasaga0 <dragoniasaga0@gmail.com>
2025-05-21 08:11:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const PORT = process.env.PORT;
 
 const SUITS = ["♥", "♦", "♣", "♠"];
 const COLORS = { "♥": "red", "♦": "red", "♣": "black", "♠": "black" };
@@ -1045,3 +1044,9 @@ io.on("connection", (socket) => {
   }
   
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
