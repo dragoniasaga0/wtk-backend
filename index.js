93c9f4073679c95b38ec832d207629ac2745e9be
dragoniasaga0 <dragoniasaga0@gmail.com>
2025-05-21 08:05:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const SUITS = ["♥", "♦", "♣", "♠"];
 const COLORS = { "♥": "red", "♦": "red", "♣": "black", "♠": "black" };
